a105048771552ceb50f3cab866511cad547b9ee7
lolenseu <lolenseu.koleutejeu248@gmail.com>
2024-12-22 20:07:15 +0800

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 import json
 import requests
 from flask import Flask, jsonify, request
+from datetime import datetime
 
 
 # Variables
@@ -10,6 +11,21 @@ app = Flask(__name__)
 parent_folder: str = './pipeline'
 megastream_forlder: str = './megastream'
 
+
+# Response Class
+class Response:
+    @staticmethod
+    def error(message: str, pipeline_id: str, timestamp: str) -> tuple:
+        response = {'response': 'error!', 'error': message, 'id': pipeline_id, 'timestamp': timestamp}
+        return jsonify(response), 400
+
+    @staticmethod
+    def success(message: str, pipeline_id: str, timestamp: str, data: dict = None) -> tuple:
+        response = {'response': 'success!', 'message': message, 'id': pipeline_id, 'timestamp': timestamp}
+        if data:
+            response.update(data)
+        return jsonify(response), 201
+    
 
 # Functions
 def create_id(pipeline_id: str, pipeline_key: str) -> bool:    
@@ -50,16 +66,16 @@ def data_available(pipeline_id: str) -> bool:
     file_path = os.path.join(parent_folder, hex_folder_name, 'stream.json')
     return os.path.exists(file_path)
 
-def store_data(id: str, data: dict) -> None:
-    hex_folder_name = hex(int(id))
+def store_data(pipeline_id: str, data: dict) -> None:
+    hex_folder_name = hex(int(pipeline_id))
     folder_location = os.path.join(parent_folder, hex_folder_name)
     if not os.path.exists(folder_location):
         os.makedirs(folder_location)
     with open(os.path.join(folder_location, 'stream.json'), 'w') as f:
         json.dump(data, f)
 
-def read_data(id: str) -> dict:
-    hex_folder_name = hex(int(id))
+def read_data(pipeline_id: str) -> dict:
+    hex_folder_name = hex(int(pipeline_id))
     folder_location = os.path.join(parent_folder, hex_folder_name)
     with open(os.path.join(folder_location, 'stream.json'), 'r') as f:
         return json.load(f)
@@ -81,118 +97,114 @@ def home():
     response = {'message': 'Welcome to Pair-Pipeline!'}
     return jsonify(response), 200
 
+# Megastream
 @app.route('/megastream', methods=['GET', 'POST'])
 def megastream():
     id: int = total_id()
     server_ip: str = get_public_ip()
+    user_ip = request.remote_addr
+    timestamp = datetime.now().isoformat()
     megasteam: dict = {
         'server_ip': server_ip,
-        'total_id': id
-        }
+        'user_ip': user_ip,
+        'total_id': id,
+        'timestamp': timestamp
+    }
     return jsonify(megasteam), 200
 
-@app.route('/pipeline/create', methods=['GET', 'POST'])
-def pipeline_create():
+# Pipeline
+@app.route('/pipeline', methods=['GET', 'POST'])
+def pipeline():
     try:
+        pipeline_option: str = request.args.get('opt')
         pipeline_id: str = request.args.get('id')
         pipeline_key: str = request.args.get('key')
-        
-        if confirm_id(pipeline_id):
-            response = {'error': 'ID already exists'}
-            return jsonify(response), 400    
-        else:
-            create_id(pipeline_id, pipeline_key)
-            response = {'message': 'Pipeline created successfully', 'id': pipeline_id}
-            return jsonify(response), 201   
-    except:
-        response = {'error': 'Invalid request'}
-        return jsonify(response), 400
-
-@app.route('/pipeline/send', methods=['GET', 'POST'])
-def pipeline_send():
-    try:
-        pipeline_id: str = request.args.get('id')
-        pipeline_key: str = request.args.get('key')   
-        int_virtual_data: dict[str, int] = {}
-        str_virtual_data: dict[str, str] = {}
+        timestamp = datetime.now().isoformat()
         response = {}
-             
+        
         # Check if pipeline_id is exactly 8 digits long
         if len(pipeline_id) != 8 and pipeline_id.isdigit():
-            response = {'error': 'ID must be exactly 8 digits long'}
-            return jsonify(response), 400           
+            return Response.error('ID must be exactly 8 digits long', pipeline_id, timestamp)         
         
         # check if pipeline_key
         if len(pipeline_id) != 16 and pipeline_key != confirm_key(pipeline_id):
-            response = {'error': 'Wrong key'}
-            return jsonify(response), 400
-
-        # Populate int_virtual_data
-        for i in range(1, 17):
-            key = f'ivd{i}'
-            value = request.args.get(key)
-            if value:
-                int_virtual_data[key] = int(value)
-
-        # Populate str_virtual_data
-        for i in range(1, 5):
-            key = f'svd{i}'
-            value = request.args.get(key)
-            if value:
-                str_virtual_data[key] = value
-
-        for x in int_virtual_data:
-            int_count: int = 4
-            if len(str(int_virtual_data[x])) > int_count:
-                response = {'error': f'Integer limit {int_count} digits for {x}'}
-                return jsonify(response), 400
-
-        for x in str_virtual_data:
-            str_count: int = 256
-            if len(str_virtual_data[x]) > str_count:
-                response = {'error': f'String limit {str_count} characters for {x}'}
-                return jsonify(response), 400
-
-        if int_virtual_data or str_virtual_data:
-            store_data(pipeline_id, {**int_virtual_data, **str_virtual_data})
-            response = {'message': 'Data stored successfully', 'id': pipeline_id}
-            return jsonify(response), 201
-        else:
-            response = {'error': 'Data not stored', 'id': pipeline_id}
-            return jsonify(response), 201    
-    except:
-        response = {'error': 'Invalid request'}
-        return jsonify(response), 400
-
-@app.route('/pipeline/receive', methods=['GET', 'POST'])
-def pipeline_receive():
-    try:
-        pipeline_id: str = request.args.get('id')
-        pipeline_key: str = request.args.get('key')
+            return Response.error('Wrong key', pipeline_id, timestamp)
         
-        if confirm_id(pipeline_id):
-            if confirm_key(pipeline_id) != pipeline_key:
-                response = {'error': 'Wrong key'}
-                return jsonify(response), 400
-            
-            if data_available(pipeline_id):
-                data = read_data(pipeline_id)
-                response = {'message': 'Data retrieved successfully', 'id': pipeline_id, 'stream': data}
+        # Check if pipeline_option is valid
+        if pipeline_option == 'cre':
+            if confirm_id(pipeline_id):
+                return Response.error('ID already exists', pipeline_id, timestamp)  
             else:
-                response = {'error': 'Data not retrieved'}
-            
-            return jsonify(response), 200
-        else:
-            response = {'error': 'ID not found'}
-            return jsonify(response), 400
-    except:
-        response = {'error': 'Invalid request'}
-        return jsonify(response), 400
+                create_id(pipeline_id, pipeline_key)
+                return Response.success('Pipeline created successfully', pipeline_id, timestamp)
+        
+        elif pipeline_option == 'snd':
+            if confirm_id(pipeline_id):  
+                int_virtual_data: dict[str, int] = {}
+                str_virtual_data: dict[str, str] = {}
+                
+                # Populate int_virtual_data
+                for i in range(len(int_virtual_data)):
+                    key = f'ivd{i}'
+                    value = request.args.get(key)
+                    if value:
+                        if not value.isdigit() or int(value) > 9999:
+                            return Response.error(f'Integer limit 4 digits for {key}', pipeline_id, timestamp)
+                        int_virtual_data[key] = int(value)
+                
+                # Check if the number of ivd values exceeds the limit
+                if len(int_virtual_data) > 16:
+                    return Response.error('Exceeded limit of 16 ivd values', pipeline_id, timestamp)
 
+                # Populate str_virtual_data
+                for i in range(len(str_virtual_data)):
+                        key = f'svd{i}'
+                        value = request.args.get(key)
+                        if value:
+                            if len(value) > 128:
+                                response = {'response': 'error!', 'error': f'String limit 128 characters for {key}'}
+                                return jsonify(response), 400
+                            str_virtual_data[key] = value
+                        
+                # Check if the number of svd values exceeds the limit
+                if len(str_virtual_data) > 4:
+                    return Response.error('Exceeded limit of 4 svd values', pipeline_id, timestamp)
+                
+                data = {
+                    'int_virtual_data': int_virtual_data,
+                    'str_virtual_data': str_virtual_data,
+                    'timestamp': timestamp
+                }
+
+                if int_virtual_data or str_virtual_data:
+                    store_data(pipeline_id, data)
+                    return Response.success('Data stored successfully', pipeline_id, timestamp)
+                else:
+                    return Response.error('Data not stored', pipeline_id, timestamp)
+            else:
+                return Response.error('ID not found', pipeline_id, timestamp)
+            
+        elif pipeline_option == 'rcv':
+            if confirm_id(pipeline_id):
+                if data_available(pipeline_id):
+                    data = read_data(pipeline_id)
+                    return Response.success('Data retrieved successfully', pipeline_id, timestamp, {'stream': data})
+                else:
+                    return Response.error('Data not retrieved', pipeline_id, timestamp)
+            else:
+                return Response.error('ID not found', pipeline_id, timestamp)
+                       
+        else: 
+            return Response.error('Invalid request', pipeline_id, timestamp)
+        
+    except:
+        return Response.error('Internal server error', pipeline_id, timestamp)
+
+# Error Handling
 @app.errorhandler(404)
 def page_not_found(e):
-    response = {'error': 'Invalid request'}
-    return jsonify(response), 404
+    timestamp = datetime.now().isoformat()
+    return Response.error('Invalid request', '', timestamp)
 
 
 # Main
